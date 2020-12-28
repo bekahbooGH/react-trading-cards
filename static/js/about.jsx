f55e513ca975414de21ad753c9e9cbececdc5cbe
vagrant <vagrant@vagrant.vm>
2020-12-28 20:12:03 +0000

    Added about files.

---
+++ static/js/about.jsx
@@ -0,0 +1,12 @@
+"use strict";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About! Who knows!!</h1>
+     
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<About />, document.querySelector('#about'));
